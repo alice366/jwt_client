9a19e2c42c2c5748a878169eafd98462d0dcd7a9
anicieja <alice.nicieja@gmail.com>
2020-12-28 14:38:57 +0100

    init commit

--- src/main/java/uk/demo/jwt_client/BookApiClient.java
+++ src/main/java/uk/demo/jwt_client/BookApiClient.java
@@ -10,22 +10,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 import java.util.stream.Stream;
 
 @Controller
 public class BookApiClient {
 
+    private String jwt;
+    private MultiValueMap<String, String> headers;
+
     public BookApiClient() {
-        addBooks();
+        jwt = getJwtToken(true);
+        headers = setStringStringMultiValueMap();
+        String bookName = "Spring Batch in action";
+        addBook(bookName);
         getBooks();
-
     }
-    private void addBooks() {
-        String jwt = getJwtToken(true);
 
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + jwt);
-        String bookToAdd = "Spring in action";
+    private void addBook(String bookToAdd) {
+
         HttpEntity httpEntity = new HttpEntity(bookToAdd, headers);
 
         RestTemplate restTemplate = new RestTemplate();
@@ -35,11 +44,14 @@ public class BookApiClient {
                 Void.class);
     }
 
-    private void getBooks() {
-        String jwt = getJwtToken(true);
-
+    private MultiValueMap<String, String> setStringStringMultiValueMap() {
         MultiValueMap<String, String> headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + jwt);
+        return headers;
+    }
+
+    private void getBooks() {
+
         HttpEntity httpEntity = new HttpEntity(headers);
 
         RestTemplate restTemplate = new RestTemplate();
@@ -52,10 +64,31 @@ public class BookApiClient {
     }
 
     private String getJwtToken(boolean isAdmin) {
-        Algorithm algorithm = Algorithm.HMAC512("7w!z%C*F-J@NcRfUjXn2r5u8x/A?D(G+KbPdSgVkYp3s6v9y$B&E)H@McQfThWmZ");
-        String jwt = JWT.create()
+
+        Algorithm algorithm = Algorithm.RSA256(null, (RSAPrivateKey) getPrivateKey());
+        return JWT.create()
                 .withClaim("admin", isAdmin)
                 .sign(algorithm);
-        return jwt;
     }
+
+    private PrivateKey getPrivateKey() {
+
+        String privateKey = "MIIBVQIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAw1BQvLKLOzk0II+e" +
+                "J9wBLjV2IjehnNSvS2klTpMF3Dy1EC3eY3ZmxiQ8rrSaOMwOsXA5hdf290kecdIj" +
+                "5u6TRQIDAQABAkEAkbeXNOFETWAlSvG7fmN+ofoS8/5rXfWz/uAojFHWenOlzUau" +
+                "mAhdFjwNcIdc8qkfojrQzHd8wHTkWLqGK0bpwQIhAPENVbdlJd+npin70txczY+P" +
+                "/TRUqhqj5VOGUjSKxXPRAiEAz2zkHzA/bBAlWbUntuKgnRJjmIH9pir41Vig2OJX" +
+                "STUCIC8HPPvkvfjeimqSeNcJPAmQPAQjqHQ+GZWsFQmvMUqhAiEAwlqWeT68/mU2" +
+                "ihK6zpsUwXg8h+atI2i6VTVBKVcTUE0CICDTNIzA8pGo6fkLN4K7zrF5KwptKPYA" +
+                "NIQXPVtvmCuU";
+
+        PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey));
+        try {
+            return  KeyFactory.getInstance("RSA").generatePrivate(encodedKeySpec);
+        } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
